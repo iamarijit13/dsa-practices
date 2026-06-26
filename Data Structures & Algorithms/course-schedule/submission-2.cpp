94752dc544ba797227e1ee25a4c7dfbb1c9984f0
Arijit Pramanik <81533583+iamarijit13@users.noreply.github.com>
2026-06-26 08:10:26 +0530

    Add: course-schedule - submission-2

---
+++ Data Structures & Algorithms/course-schedule/submission-2.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_map<int, vector<int>> hash;
+        unordered_set<int> visit;
+        
+        for (auto& pre : prerequisites) {
+            if (!hash.count(pre[0])) {
+                hash[pre[0]] = {pre[1]};
+            } else {
+                hash[pre[0]].push_back(pre[1]);
+            }
+        }
+
+        for (int c = 0; c < numCourses; c++) {
+            if (!dfs(hash, visit, c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool dfs(unordered_map<int, vector<int>>& hash, unordered_set<int>& visit, int course) {
+        if (visit.count(course)) {
+            return false;
+        }
+
+        if (hash[course].empty()) {
+            return true;
+        }
+        visit.insert(course);
+
+        for (auto& h : hash[course]) {
+            if (!dfs(hash, visit, h)) {
+                return false;
+            }
+        }
+        hash[course] = {};
+        visit.erase(course);
+        return true;
+    }
+};
