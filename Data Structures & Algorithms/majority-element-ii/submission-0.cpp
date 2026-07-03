66e4fe07784acbd6ecfd99d56a9f0ca8b5d4f19a
Arijit Pramanik <81533583+iamarijit13@users.noreply.github.com>
2026-07-03 10:19:49 +0530

    Add: majority-element-ii - submission-0

---
+++ Data Structures & Algorithms/majority-element-ii/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        unordered_map<int, int> hash;
+        vector<int> re;
+
+        for (const auto& n : nums) {
+            if (hash.count(n)) {
+                hash[n]++;
+            } else {
+                hash.insert({n, 1});
+            }
+        }
+
+        for (const auto& [k, v] : hash) {
+            if (v > ceil(nums.size() / 3)) {
+                re.push_back(k);
+            }
+        }
+
+        return re;
+    }
+};
