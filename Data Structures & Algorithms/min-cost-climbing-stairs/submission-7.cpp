b7c45cbeebf5d579edfe89e69bdb715c6a65b92c
Arijit Pramanik <81533583+iamarijit13@users.noreply.github.com>
2026-07-18 10:35:39 +0530

    Add: min-cost-climbing-stairs - submission-7

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-7.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int minCostClimbingStairss(vector<int>& cost) {
+        cost.push_back(0);
+        int size = cost.size();
+
+        for (int i = size - 3; i >= 0; i--) {
+            cost[i] = cost[i] + min(cost[i + 1], cost[i + 2]);
+        }
+        return min(cost[0], cost[1]);
+    }
+
+    int minCostClimbingStairs(vector<int>& cost) {
+        int size = cost.size();
+        unordered_map<int, int> hash;
+
+        int cost1 = climb(cost, 0, hash);
+        int cost2 = climb(cost, 1, hash);
+        return min(cost1, cost2);
+    }
+
+    int climb(vector<int>& cost, int index, unordered_map<int, int>& hash) {
+        if (hash.count(index)) {
+            return hash[index];
+        }
+
+        if (index >= cost.size()) {
+            return 0;
+        }
+
+        int cost1 = climb(cost, index + 1, hash);
+        int cost2 = climb(cost, index + 2, hash);
+
+        return hash[index] = cost[index] + min(cost1, cost2);
+    }
+};
+  
