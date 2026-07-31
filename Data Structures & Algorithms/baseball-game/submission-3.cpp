aa8c4151f938d9493426c445e02a2769d21e93b3
Arijit Pramanik <81533583+iamarijit13@users.noreply.github.com>
2026-07-31 09:30:54 +0530

    Add: baseball-game - submission-3

---
+++ Data Structures & Algorithms/baseball-game/submission-3.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        stack<int> st;
+        int re = 0;
+
+        for (string op : operations) {
+            if (op == "+") {
+                if (!st.empty() && st.size() >= 2) {
+                    int p1 = st.top();
+                    st.pop();
+                    int p2 = st.top();
+                    // st.pop();
+                    st.push(p1);
+                    // st.push(p2);
+                    st.push(p1 + p2);
+                }
+            } else if (op == "D") {
+                if (!st.empty()) {
+                    int p1 = st.top();
+                    st.push(p1 * 2);
+                }
+            } else if (op == "C") {
+                if (!st.empty()) {
+                    st.pop();
+                }
+            } else {
+                st.push(stoi(op));
+            }
+        }
+
+        while (!st.empty()) {
+            re += st.top();
+            st.pop();
+        }
+
+        return re;
+    }
+
+    int calPointss(vector<string>& ops) {
+        stack<int> stack;
+        int res = 0;
+        for (const string& op : ops) {
+            if (op == "+") {
+                int top = stack.top(); stack.pop();
+                int newTop = top + stack.top();
+                stack.push(top);
+                stack.push(newTop);
+                res += newTop;
+            } else if (op == "D") {
+                stack.push(2 * stack.top());
+                res += stack.top();
+            } else if (op == "C") {
+                res -= stack.top();
+                stack.pop();
+            } else {
+                stack.push(stoi(op));
+                res += stack.top();
+            }
+        }
+        return res;
+    }
+};
