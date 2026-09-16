40a2b0298691eaa1e966f74677f78a12bb20000a
Arijit Pramanik <81533583+iamarijit13@users.noreply.github.com>
2026-09-16 17:25:33 +0530

    Add: hand-of-straights - submission-3

---
+++ Data Structures & Algorithms/hand-of-straights/submission-3.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        if (hand.size() % groupSize != 0) return false;
+
+        unordered_map<int, int> hash;
+        priority_queue<int, vector<int>, greater<int>> q;
+        for (int h : hand) {
+            if (hash[h] == 0) {
+                q.push(h);
+            }
+            hash[h]++;
+        }
+
+        while (!q.empty()) {
+            int min = q.top();
+            int next = min;
+            for (int i = 1; i <= groupSize; i++) {
+                if (hash[next] > 0) {
+                    hash[next]--;
+                    if (hash[next] == 0) q.pop();
+                } else {
+                    return false;
+                }
+                next++;
+            }
+        }
+
+        return true;
+    }
+};
