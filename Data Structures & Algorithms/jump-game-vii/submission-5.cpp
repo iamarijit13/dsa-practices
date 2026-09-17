a1797357345c0dbdcddf0e3e78667a912c403bc5
Arijit Pramanik <81533583+iamarijit13@users.noreply.github.com>
2026-09-17 09:08:43 +0530

    Add: jump-game-vii - submission-5

---
+++ Data Structures & Algorithms/jump-game-vii/submission-5.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool canReach(string s, int minJump, int maxJump) {
+        if (s.back() == '1') return false;
+        queue<int> q;
+        int farthest = 0;
+        q.push(0);
+
+        while (!q.empty()) {
+            int index = q.front();
+            q.pop();
+            int start = max(index + minJump, farthest + 1);
+            int end = min((index + maxJump + 1), (int)s.size());
+            // int end = index + maxJump + 1;
+            for (int i = start; i < end; i++) {
+                // if (i >= s.size()) break;
+                if (s[i] == '0') q.push(i);
+                if (i == s.size() - 1) return true;
+            }
+            farthest = index + maxJump;
+        }
+        return false;
+    }
+};
